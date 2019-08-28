25f78358424a9a520685f4adf680fe6412b4cc2c
DipsyTaurus <451480726@qq.com>
2019-08-28 19:32:18 +0800

    huimin

---
+++ packages/index.js
@@ -0,0 +1,18 @@
+import taurusfirst from './taurusfirst'
+const components = [
+    taurusfirst
+]
+
+const install = function (Vue) {
+    if(install.installed) return
+    components.map(component => Vue.component(component.name,component))
+}
+
+if (typeof window !== 'undefined' && window.Vue){
+    install(window.Vue)
+}
+
+export default {
+    install,
+    taurusfirst
+}
---
+++ packages/taurusfirst/index.js
@@ -0,0 +1,6 @@
+import taurusfirst from './src/taurusfirst.vue'
+taurusfirst.install = function (Vue) {
+    Vue.component(taurusfirst.name,taurusfirst)
+}
+
+export default taurusfirst
